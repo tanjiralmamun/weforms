cda8e46234fbcc6eb56170d629a1487520e02306
Dan Cameron <dan@sproutventure.com>
2020-09-30 07:43:17 -0700

    Sprout Invoices Integration Update
    * Handle Merge Tags
    * Improve Notes for the Submission
    * Improve code
    
    Fix #34

--- includes/integrations/sprout-invoices/class-integration-sprout-invoices.php
+++ includes/integrations/sprout-invoices/class-integration-sprout-invoices.php
@@ -34,10 +34,20 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
             ],
         ];
 
-        add_action( 'weforms_entry_submission', [$this, 'create_doc'], 10, 2 );
+        add_action( 'weforms_entry_submission', [$this, 'create_doc'], 10, 3 );
         add_filter( 'weforms_entry_submission_response', [$this, 'modify_response_data_redirect'] );
     }
 
+
+    /**
+     * Check if the dependency met
+     *
+     * @return bool
+     */
+    public function has_dependency()
+    {
+        return function_exists( 'sprout_invoices_load' );
+    }
 
     public function modify_response_data_redirect($response_data)
     {
@@ -57,7 +67,7 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
      *
      * @return void
      */
-    public function create_doc($entry_id, $form_id)
+    public function create_doc($entry_id, $form_id, $page_id)
     {
         if (!$this->has_dependency()) {
             return;
@@ -73,7 +83,7 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
         if (false === $form_data) {
             return;
         }
-        $address = self::get_array( $integration->fields->address, $entry_id );
+        $address = self::get_value( $integration->fields->address, $entry_id, $form_id, $page_id );
         if (is_array( $address )) {
             $full_address = array(
                 'street' => isset( $address['street_address'] ) ? $address['street_address'] . ' ' . $address['street_address2'] : '',
@@ -84,7 +94,7 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
             );
         }
 
-        /*$line_items = self::get_array( $integration->fields->line_items, $entry_id );
+        /*$line_items = self::get_value( $integration->fields->line_items, $entry_id, $form_id, $page_id );
         if (is_array($line_items)) {
             foreach ($line_items as $rate => $value) {
                 $li[] = array(
@@ -98,39 +108,45 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
 
         //Setting up array to send user info to WordPress
         $submission = array(
-            'subject' => self::maybe_name( $integration->fields->subject, $entry_id ),
+            'subject' => self::get_value( $integration->fields->subject, $entry_id, $form_id, $page_id ),
             //'line_items' => ! empty( $li ) ? $li : array() ,
             'full_address' => !empty( $full_address ) ? $full_address : array(),
-            'client_name' => self::maybe_name( $integration->fields->client_name, $entry_id ),
-            'email' => WeForms_Notification::replace_field_tags( $integration->fields->email, $entry_id ),
-            'first_name' => self::maybe_name( $integration->fields->first_name, $entry_id ),
-            'last_name' => self::maybe_name( $integration->fields->last_name, $entry_id ),
-            'notes' => WeForms_Notification::replace_field_tags( $integration->fields->notes, $entry_id ),
-            'duedate' => WeForms_Notification::replace_field_tags( $integration->fields->duedate, $entry_id ),
-            'number' => WeForms_Notification::replace_field_tags( $integration->fields->number, $entry_id ),
-            'vat' => WeForms_Notification::replace_field_tags( $integration->fields->vat, $entry_id ),
-            'entry_note' => self::get_fields_table( $form_id, $entry_id ),
-            'edit_url' => admin_url( sprintf( 'admin.php?page=weforms#/form/%s/entries/%s', $form_id, $entry_id ) ),
+            'client_name' => self::get_value( $integration->fields->client_name, $entry_id, $form_id, $page_id ),
+            'email' => self::get_value( $integration->fields->email, $entry_id, $form_id, $page_id ),
+            'first_name' => self::get_value( $integration->fields->first_name, $entry_id, $form_id, $page_id ),
+            'last_name' => self::get_value( $integration->fields->last_name, $entry_id, $form_id, $page_id ),
+            'notes' => self::get_value( $integration->fields->notes, $entry_id, $form_id, $page_id ),
+            'duedate' => self::get_value( $integration->fields->duedate, $entry_id, $form_id, $page_id ),
+            'number' => self::get_value( $integration->fields->number, $entry_id, $form_id, $page_id ),
+            'vat' => self::get_value( $integration->fields->vat, $entry_id, $form_id, $page_id ),
+            'entry_note' => self::build_entry_note( $form_id, $entry_id ),
+            'entry_id' => $entry_id,
+            'form_id' => $form_id,
+            'page_id' => $page_id,
+            'form_data' => $form_data['data'],
         );
+
+        error_log( "submission: " . print_r( $submission, TRUE ) );
+
 
         $doc_id = 0;
         $doctype = $integration->doctype;
         $create_user_and_client = $integration->create_user_and_client;
         switch ($doctype) {
             case 'invoice':
-                $doc_id = $this->create_invoice( $submission, $form_data['data'], $form_id );
+                $doc_id = $this->create_invoice( $submission );
                 if ($create_user_and_client) {
-                    $this->create_client( $submission, $form_data['data'], $doc_id, $form_id );
+                    $this->create_client( $submission, $doc_id );
                 }
                 break;
             case 'estimate':
-                $doc_id = $this->create_estimate( $submission, $form_data['data'], $form_id );
+                $doc_id = $this->create_estimate( $submission );
                 if ($create_user_and_client) {
-                    $this->create_client( $submission, $form_data['data'], $doc_id, $form_id );
+                    $this->create_client( $submission, $doc_id );
                 }
                 break;
             case 'client':
-                $this->create_client( $submission, $form_data['data'], 0, $form_id );
+                $this->create_client( $submission );
                 break;
             default:
                 // nada
@@ -154,17 +170,34 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
 
     }
 
-    /**
-     * Check if the dependency met
-     *
-     * @return bool
-     */
-    public function has_dependency()
+    public function get_value($text, $entry_id, $form_id, $page_id)
     {
-        return function_exists( 'sprout_invoices_load' );
+
+        // basic field
+        $value = self::get_meta_value( $text, $entry_id );
+        if (false != $value) {
+            return $value;
+        }
+
+        // name field
+        $value = self::maybe_name( $text, $entry_id );
+        if (false != $value) {
+            return $value;
+        }
+
+        $notification = new WeForms_Notification( [
+            'form_id'  => $form_id,
+            'page_id'  => $page_id,
+            'entry_id' => $entry_id,
+        ] );
+
+        // merge field
+        $value = $notification->get_merge_value( preg_replace('/[{}]/', '', $text) );
+
+        return $value;
     }
 
-    private static function get_array($text, $entry_id)
+    private static function get_meta_value($text, $entry_id)
     {
         $pattern = '/{field:(\w*)}/';
 
@@ -172,10 +205,10 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
 
         // bail out if nothing found to be replaced
         if (!$matches) {
-            return $text;
+            return false;
         }
 
-        // returning the first address, can't really deal with more.
+        // returning the first value, can't really deal with more.
         foreach ($matches[1] as $index => $meta_key) {
             return weforms_get_entry_meta( $entry_id, $meta_key, true );
         }
@@ -189,7 +222,7 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
 
         // bail out if nothing found to be replaced
         if (!$matches[0]) {
-            return WeForms_Notification::replace_field_tags( $text, $entry_id );
+            return false;
         }
 
         list( $search, $fields, $meta_key ) = $matches;
@@ -212,14 +245,73 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
         return $text;
     }
 
-    protected function create_invoice($submission = array(), $entry = array(), $form_id = 0)
+    public static function build_entry_note($form_id, $entry_id)
+    {
+
+        $form = weforms()->form->get( $form_id );
+        $entry = $form->entries()->get( $entry_id );
+        $fields = $entry->get_fields();
+
+        if (!$fields) {
+            return '';
+        }
+
+        $table .= '<div class="submission-values">';
+        $table .= '<ul>';
+
+        foreach ($fields as $key => $value) {
+            $field_value = isset( $value['value'] ) ? $value['value'] : '';
+
+            if (!$field_value) {
+                continue; // let's skip empty fields
+            }
+
+            $table .= '<li>';
+
+            $table .= '<span class="submission-label"><strong>' . $value['label'] . ':&nbsp;</strong></span>';
+
+            $table .= '<span class="submission-value">';
+            if (in_array( $value['type'], ['multiple_select', 'checkbox_field'] )) {
+                $field_value = is_array( $field_value ) ? $field_value : [];
+
+                if ($field_value) {
+                    $table .= '<ul>';
+
+                    foreach ($field_value as $value_key) {
+                        $table .= '<li>' . $value_key . '</li>';
+                    }
+                    $table .= '</ul>';
+                } else {
+                    $table .= '&mdash;';
+                }
+            } else {
+                $table .= $field_value;
+            }
+            $table .= '</span>';
+
+            $table .= '</li>';
+
+        }
+
+        $table .= '</ul>';
+        $table .= '</div>';
+
+
+        $table .= '<div>';
+        $edit_url = admin_url( sprintf( 'admin.php?page=weforms#/form/%s/entries/%s', $form_id, $entry_id ) );
+        $table .=  sprintf( __( 'Invoice Submitted: Form <a href="%s">#%s</a>.', 'sprout-invoices' ), $edit_url, $entry_id );
+        $table .= '</div>';
+
+        return $table;
+    }
+
+    protected function create_invoice($submission = array())
     {
 
         $invoice_args = array(
             'subject' => sprintf( apply_filters( 'si_form_submission_title_format', '%1$s (%2$s)', $submission ), $submission['subject'], $submission['client_name'] ),
             'fields' => $submission,
-            'form' => $entry,
-            'history_link' => '<a href="' . $submission['edit_url'] . '">#' . $form_id . '</a>',
+            'form' => $submission['form_data'],
         );
 
         do_action( 'si_doc_generation_start' );
@@ -237,8 +329,6 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
             SI_Internal_Records::new_record( $submission['notes'], SI_Controller::PRIVATE_NOTES_TYPE, $invoice_id, '', 0, false );
         }
 
-        SI_Internal_Records::new_record( $submission['entry_note'], SI_Controller::PRIVATE_NOTES_TYPE, $invoice_id, '', 0, false );
-
         if (isset( $submission['number'] )) {
             $invoice->set_invoice_id( $submission['number'] );
         }
@@ -248,25 +338,17 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
         }
 
         // Finally associate the doc with the form submission
-        add_post_meta( $invoice_id, 'wef_form_id', $form_id );
+        add_post_meta( $invoice_id, 'wef_form_id', $submission['form_id'] );
 
-        $history_link = sprintf( '<a href="%s">#%s</a>', $submission['edit_url'], $form_id );
+        SI_Internal_Records::new_record( $submission['entry_note'], 'invoice_submission', $invoice_id, sprintf( __( 'Invoice Submitted: Form %s.', 'sprout-invoices' ), $submission['entry_id'] ), '', false );
 
-        do_action( 'si_new_record',
-            sprintf( __( 'Invoice Submitted: Form %s.', 'sprout-invoices' ), $history_link ),
-            'invoice_submission',
-            $invoice_id,
-            sprintf( __( 'Invoice Submitted: Form %s.', 'sprout-invoices' ), $history_link ),
-            0,
-            false );
-
-        do_action( 'si_invoice_submitted_from_adv_form', $invoice, $invoice_args, $submission, $entry );
+        do_action( 'si_invoice_submitted_from_adv_form', $invoice, $invoice_args, $submission, $submission['form_data'] );
 
         return $invoice_id;
 
     }
 
-    protected function create_client($submission = array(), $entry = array(), $doc_id = 0, $form_id = 0)
+    protected function create_client($submission = array(), $doc_id = 0)
     {
 
         $email = $submission['email'];
@@ -331,13 +413,12 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
 
     }
 
-    protected function create_estimate($submission = array(), $entry = array(), $form_id)
+    protected function create_estimate($submission = array())
     {
         $estimate_args = array(
             'subject' => sprintf( apply_filters( 'si_form_submission_title_format', '%1$s (%2$s)', $submission ), $submission['subject'], $submission['client_name'] ),
             'fields' => $submission,
-            'form' => $submission,
-            'history_link' => $submission['edit_url'],
+            'form' => $submission['form_data'],
         );
 
         do_action( 'si_doc_generation_start' );
@@ -355,7 +436,6 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
         if (isset( $submission['notes'] ) && '' !== $submission['notes']) {
             SI_Internal_Records::new_record( $submission['notes'], SI_Controller::PRIVATE_NOTES_TYPE, $estimate_id, '', 0, false );
         }
-        SI_Internal_Records::new_record( $submission['entry_note'], SI_Controller::PRIVATE_NOTES_TYPE, $estimate_id, '', 0, false );
 
         if (isset( $submission['number'] )) {
             $estimate->set_estimate_id( $submission['number'] );
@@ -366,72 +446,11 @@ class WeForms_Integration_SI extends WeForms_Abstract_Integration
         }
 
         // Finally associate the doc with the form submission
-        add_post_meta( $estimate_id, 'wef_form_id', $form_id );
+        add_post_meta( $estimate_id, 'wef_form_id', $submission['form_id'] );
 
-        $history_link = '<a href="' . $submission['edit_url'] . '">#' . $form_id . '</a>';
+        SI_Internal_Records::new_record( $submission['entry_note'], 'estimate_submission', $estimate_id, sprintf( __( 'Estimate Submitted: Form %s.', 'sprout-invoices' ), $submission['entry_id'] ), '', false );
 
-        do_action( 'si_new_record',
-            sprintf( __( 'Estimate Submitted: Form %s.', 'sprout-invoices' ), $history_link ),
-            'estimate_submission',
-            $estimate_id,
-            sprintf( __( 'Estimate Submitted: Form %s.', 'sprout-invoices' ), $history_link ),
-            0,
-            false );
 
         return $estimate_id;
-    }
-
-    public static function get_fields_table($form_id, $entry_id)
-    {
-
-        $form = weforms()->form->get( $form_id );
-        $entry = $form->entries()->get( $entry_id );
-        $fields = $entry->get_fields();
-
-        if (!$fields) {
-            return '';
-        }
-
-        $table = '<table width="600" cellpadding="0" cellspacing="0">';
-        $table .= '<tbody>';
-
-        foreach ($fields as $key => $value) {
-            $field_value = isset( $value['value'] ) ? $value['value'] : '';
-
-            if (!$field_value) {
-                continue; // let's skip empty fields
-            }
-
-            $table .= '<tr class="field-label">';
-            $table .= '<th><strong>' . $value['label'] . '</strong></th>';
-            $table .= '</tr>';
-            $table .= '<tr class="field-value">';
-            $table .= '<td>';
-
-            if (in_array( $value['type'], ['multiple_select', 'checkbox_field'] )) {
-                $field_value = is_array( $field_value ) ? $field_value : [];
-
-                if ($field_value) {
-                    $table .= '<ul>';
-
-                    foreach ($field_value as $value_key) {
-                        $table .= '<li>' . $value_key . '</li>';
-                    }
-                    $table .= '</ul>';
-                } else {
-                    $table .= '&mdash;';
-                }
-            } else {
-                $table .= $field_value;
-            }
-
-            $table .= '</td>';
-            $table .= '</tr>';
-        }
-
-        $table .= '</tbody>';
-        $table .= '</table>';
-
-        return $table;
     }
 }
